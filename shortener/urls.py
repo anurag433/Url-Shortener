6bad16a4bbf8aecddc1b5714fe124629d70fb3bb
Anurag Singh <anurag504singh@gmail.com>
2026-03-19 01:07:11 +0530

    feat: add redirect logic with Redis caching

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,8 +1,9 @@
 # shortener/urls.py
 
 from django.urls import path
-from .views import CreateShortURLView
+from .views import CreateShortURLView, RedirectView
 
 urlpatterns = [
     path('shorten/', CreateShortURLView.as_view()),
+    path('<str:short_code>/',RedirectView.as_view()),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.shortcuts import redirect
+from django.http import Http404
+from .models import ShortURL
 from .serializers import ShortURLSerializer
 from .services import create_short_url
+from core.cache import get_from_cache, set_to_cache
 
 class CreateShortURLView(APIView):
 
@@ -14,8 +17,30 @@ class CreateShortURLView(APIView):
             obj = create_short_url(
                 serializer.validated_data['original_url']
             )
+
+            short_url = request.build_absolute_uri(f"/{obj.short_code}")
             return Response({
-                "short_code": obj.short_code
+                "short_url": short_url
             })
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
+    
+class RedirectView(APIView):
+    def get(self, request,short_code):
+        original_url = get_from_cache(short_code)
+        if original_url:
+            return redirect(original_url)
+        
+        try:
+            obj = ShortURL.objects.get(short_code=short_code)
+            set_to_cache(short_code, obj.original_url)
+            obj.clicks +=1 
+            obj.save()
+
+            return redirect(obj.original_url)
+
+        
+        except ShortURL.DoesNotExist:
+            raise Http404("Url not found !")
