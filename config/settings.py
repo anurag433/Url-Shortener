0480727bbf271b7b3c32ba3b59f3c993d70987c7
Anurag Singh <anurag504singh@gmail.com>
2026-03-31 10:39:38 +0530

    Setup backend deployment

--- config/settings.py
+++ config/settings.py
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -119,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Connecting the Database
 
@@ -153,3 +155,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
     "http://localhost:3000",   
 ]
+
+ALLOWED_HOSTS = ["*.onrender.com"]
