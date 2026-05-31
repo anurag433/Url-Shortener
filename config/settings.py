528c24f88ceea24f2d5868a51455217c563bdd86
Anurag Singh <anurag504singh@gmail.com>
2026-05-31 14:14:31 +0530

    Configure Render PostgreSQL database connection

--- config/settings.py
+++ config/settings.py
@@ -167,11 +167,23 @@ ALLOWED_HOSTS = ['*']
 
 import dj_database_url
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
-}
+database_url = os.getenv("DATABASE_URL")
 
-
+if database_url:
+    DATABASES = {
+        "default": dj_database_url.parse(database_url)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
+    }
